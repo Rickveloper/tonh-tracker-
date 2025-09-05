d2beb00e23a6098ef5ce896b097a699a768810ed
Rickveloper <dickfryou44@gmail.com>
2025-09-05 16:10:01 -0400

    feat: TONH performers-only tracker (PWA) + Pages deploy

--- sw.js
+++ sw.js
@@ -6,6 +6,39 @@ const ASSETS = [
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+self.addEventListener('fetch', e => {
+  const { request } = e;
+  if (request.url.includes('/api/states/all')) return; // don't cache ADS-B
+  e.respondWith(
+    caches.match(request).then(cached =>
+      cached || fetch(request).then(resp => {
+        if (request.method === 'GET' && resp.status === 200) {
+          const copy = resp.clone();
+          caches.open(CACHE).then(c => c.put(request, copy));
+        }
+        return resp;
+      })
+    )
+  );
+});
+
+const CACHE = 'tonh-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
